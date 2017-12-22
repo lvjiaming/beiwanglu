41a1dfec534136518feab7c68a8c3c760a4be33a
lvjiaming <863537291@qq.com>
2017-12-22 16:31:48 +0800

    添加项目
    添加项目

---
+++ bwl/assets/Script/DataControl.js
@@ -0,0 +1,41 @@
+
+const DataControl = cc.Class({
+    statics: {
+        getInstance() {
+            if (!this.dataControl) {
+                this.dataControl = new DataControl();
+            }
+            return this.dataControl;
+        },
+    },
+    _data: null,  // 读取的数据
+    ctor() {
+        this._data = {};
+    },
+    /**
+     *  保存数据到本地
+     */
+    setData() {
+        cc.sys.localStorage.setItem("listData", JSON.stringify(this._data));
+    },
+    /**
+     *  读取数据
+     */
+    getData() {
+        this._data = JSON.parse(cc.sys.localStorage.getItem("listData"));
+        return this._data;
+    },
+    /**
+     *  添加一条数据
+     * @param name 数据的名字
+     * @param data 数据
+     */
+    addData(name, data) {
+        if (!this._data[name]) {
+            this._data[name] = [];
+        }
+        this._data[name].push(data);
+        this.setData();
+    },
+});
+cc.dataControl = DataControl.getInstance();
---
+++ bwl/assets/Script/ViewConfig.js
@@ -0,0 +1,13 @@
+
+// 页面配置
+const ViewConfig = {
+    _TableList: {},  // 表的列表
+    _ItemList: {},  // 元素的列表
+};
+
+ViewConfig.addConfig = () => {
+
+};
+
+exports = ViewConfig;
+cc.viewConfig = ViewConfig;
